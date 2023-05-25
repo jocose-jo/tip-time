7481c4ce3cff8e6733c3cd127c839bb7cba812a3
Joseph <joseph@mindswarms.com>
2023-05-24 20:35:49 -0700

    CRD for rdw games

--- bot_commands.py
+++ bot_commands.py
@@ -1,6 +1,6 @@
 from discord.ext import commands
 import discord
-from db import save_new_user
+import db
 
 
 def init_client(prefix='$'):
@@ -29,7 +29,31 @@ def add_bot_commands(client):
         await ctx.channel.send(f'{len(args)} arguments: {arguments}')
 
     @client.command(name='addme')
-    async def add_user(ctx: discord.ApplicationContext):
-        save_new_user(ctx.author)
-        await ctx.channel.send('Saved to DB')
+    async def add_user(ctx):
+        if db.save_new_user(ctx.author):
+            await ctx.channel.send('added!')
+        else:
+            await ctx.channel.send('You are already added!')
 
+    @client.command(name="addgame")
+    async def add_new_game(ctx, *args):
+        game_name = " ".join(args)
+        await ctx.channel.send(f'adding new game: {game_name}')
+        if db.add_new_rdw_game(game_name):
+            await ctx.channel.send(f'Successfully added new game: {game_name}')
+        else:
+            await ctx.channel.send('This game already exists :(')
+
+    @client.command(name="deletegame")
+    async def delete_game(ctx, *args):
+        game_name = " ".join(args)
+        if db.destroy_rdw_game(game_name):
+            await ctx.channel.send(f'Successfully deleted game: {game_name}')
+        else:
+            await ctx.channel.send('This game doesnt exist :(')
+
+    @client.command(name="games")
+    async def list_rdw_games(ctx):
+        rdw_games = db.fetch_rdw_games()
+        decorated_rdw_games = "\n".join(rdw_games)
+        await ctx.channel.send(f'Here are all the round the world games:\n{decorated_rdw_games}')
--- db.py
+++ db.py
@@ -7,8 +7,35 @@ cluster = MongoClient(MONGO_CONNECTION_URL)
 db = cluster["TipTimeTest"]
 runtime_collection = db["RunTimes"]
 user_collection = db["Users"]
+rdw_game_collection = db["RDWGames"]
 
 
 def save_new_user(user):
-    post = {"_id": user.id, "name": "Jojo Bot"}
-    user_collection.insert_one(post)
+    user_query = {"_id": user.id}
+    if user_collection.count_documents(user_query) == 0:
+        post = {"_id": user.id, "name": "Jojo Bot"}
+        user_collection.insert_one(post)
+        return True
+    return False
+
+
+def add_new_rdw_game(game_name):
+    game_query = {"lowerName": game_name.lower()}
+    if rdw_game_collection.count_documents(game_query) == 0:
+        post = {"name": game_name, "lowerName": game_name.lower()}
+        rdw_game_collection.insert_one(post)
+        return True
+    return False
+
+
+def destroy_rdw_game(game_name):
+    game_query = {"lowerName": game_name.lower()}
+    if rdw_game_collection.count_documents(game_query) > 0:
+        rdw_game_collection.delete_one(game_query)
+        return True
+    return False
+
+
+def fetch_rdw_games():
+    results = [result["name"] for result in rdw_game_collection.find({}, ["name"])]
+    return results
